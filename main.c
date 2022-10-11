3863ad70229dd7f13223779dc9131f5afb79afda
sohee0913 <thgml34567@gmail.com>
2022-10-11 16:44:08 +0900

    02

--- main.c
+++ main.c
@@ -2,22 +2,20 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void print_star(void)
-{	int i;
+void func(int x)
+{
+	
+	printf("func x is at %p\n", &x);
 
-	for (i=0;i<10;i++)
-	printf("*");
 }
 
 
 int main(int argc, char *argv[]){
 	
-	int i;
+	int x;
 	
-	print_star();
-	print_star();
-	print_star();
-	
+	printf("main x is at %p\n", &x);
+	func(x);	
 	
 	return 0;
 }
